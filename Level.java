ed7a66361f0e3c1775254e6d756ffd9be2f0cbcc
jadaf49 <jadaf49@nycstudents.net>
2025-05-06 04:08:07 +0000

    playManyTimes

--- Level.java
+++ Level.java
@@ -1,12 +1,25 @@
 public class Level
 {
+    private boolean goalReached;
+    private int points;
+
     public boolean goalReached()
     {
+        return goalReached;
+    }
 
+    public void reachGoal()
+    {
+        goalReached=true;
     }
 
     public int getPoints()
     {
+        return points;
+    }
 
+    public void setPoints(int p)
+    {
+        points=p;
     }
 }
